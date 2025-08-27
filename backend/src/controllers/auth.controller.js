bc0bb06e7f7718c8feeee1e99975f1da436edb36
Shwetang Yadav <you@example.com>
2025-08-27 10:44:54 +0530

    first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export function signup(req,res){
+    res.send("Signup Route");
+}
+export function login(req,res){
+    res.send("login Route");
+}
+export function logout(req,res){
+    res.send("logout Route");
+}
---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import authRoutes from "./routes/auth.route.js"
+
+const app= express();
+const PORT = process.env.PORT;
+
+app.use("/api/auth",authRoutes);
+
+
+
+app.listen (PORT,()=>{
+    console.log(`server is runnning on port ${PORT}`);
+});
